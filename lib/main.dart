56f30ed03193ba35d69c71e83b64d361cdb9e955
Jux salley <juxsalley@Juxs-MacBook-Air.local>
2020-12-02 08:34:31 -0800

    dice app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,58 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int numOne;
+  int numTwo;
+
+  void generate() {
+    numOne = Random().nextInt(5);
+    numTwo = Random().nextInt(5);
+    // print(numOne);
+    // print(numTwo);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              generate();
+            });
+          },
+          child: Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: [
+              Expanded(
+                //flex: 2,
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Image(
+                    image: AssetImage('images/dice${numOne + 1}.png'),
+                  ),
+                ),
+              ),
+              Expanded(
+                // flex: 2,
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Image(
+                    image: AssetImage('images/dice${numTwo + 1}.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
